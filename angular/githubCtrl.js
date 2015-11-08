082ce0851213b80deb4a1b90f8c73739226d828b
Rockie Yang <eyouyan@gmail.com>
2015-11-08 08:47:50 +0100

    github view

--- angular/githubCtrl.js
+++ angular/githubCtrl.js
@@ -1,8 +1,10 @@
 (function() {
     var app = angular.module('app', []);
 
-    var githubCtrl = function($scope, $http) {
-        $scope.username = "rockie-yang";
+    var githubCtrl = function($scope, $http, $interval) {
+        $scope.username = "angular";
+        $scope.orderByField = "stargazers_count";
+        $scope.reverseSort = true;
         var onError = function(response) {
             console.log(response);
             $scope.error = response.data;
@@ -11,8 +13,14 @@
         var getRepos = function(url) {
         	$http.get(url).then(function(response) {
         		$scope.repos = response.data;
+        		console.log($scope.repos[0]);
         	}, onError);
         }
+
+        var changeOrder = function(columnName) {
+        	$scope.orderByField = columnName;
+        }
+
 
         $scope.search = function() {
             var url = "https://api.github.com/users/" + $scope.username;
@@ -21,7 +29,20 @@
                 getRepos($scope.user.repos_url);
                 var gravatar_id = $scope.user.gravatar_id
             }, onError);
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.leftTime = 0;
+            }
         }
+
+        $scope.leftTime = 5;
+        var countdown = function() {
+            $scope.leftTime -= 1;
+            if ($scope.leftTime == 0) {
+                $scope.search();
+            }
+        }
+        var countdownInterval = $interval(countdown, 1000, 5)
     }
-    app.controller('githubCtrl', ['$scope', '$http', githubCtrl]);
+    app.controller('githubCtrl', ['$scope', '$http', '$interval', githubCtrl]);
 }());
